cb40fbb71fb8283fb79d7eec8fe3c6551816580e
liuxynb <2096321485@qq.com>
2023-09-15 11:32:57 +0800

    update fpga cpu

---
+++ CPU_Verilog/project_1.srcs/sources_1/imports/risc-V/extender.v
@@ -0,0 +1,10 @@
+//sign extend
+module extender_sign(in,out);
+    parameter in_WIDTH=12;
+    parameter out_WIDTH=32;
+    input [in_WIDTH-1:0]  in;
+    output reg [out_WIDTH-1:0] out;
+    always@(in) begin
+        out={{(out_WIDTH-in_WIDTH){in[in_WIDTH-1]}},in};
+    end
+endmodule
